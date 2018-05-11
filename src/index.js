c315865eeb97766cf38514b8cb662040580022ed
frozenborder <frozenborder@gmail.com>
2018-05-11 17:32:21 +0200

    End of section 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/layout/App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(<App />, rootEl);
 };
 
 if (module.hot) {
-  module.hot.accept("./app/layout/App", () => {
-      setTimeout(render);
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
   });
 }
 
